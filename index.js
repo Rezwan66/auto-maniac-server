5e4bd9c553daf36021d0f95ab7ddb555013032fa
Rezwan66 <shaikhrezwan66@gmail.com>
2023-10-20 03:13:29 +0600

    commented out client.connect and client.close for the server to run on the web

--- index.js
+++ index.js
@@ -25,10 +25,10 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const productCollection = client.db("automotiveDB").collection("products");
         const cartCollection = client.db("automotiveDB").collection("cart");
